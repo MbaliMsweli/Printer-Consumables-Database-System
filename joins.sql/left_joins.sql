ea31b36de8be018ed6e761328a02ebc1a9407358
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2026-01-27 14:18:26 +0200

    push

---
+++ joins.sql/left_joins.sql
@@ -0,0 +1,18 @@
+USE [SandavTonerDB] 
+
+-----creating a view to see supplier's product prices 
+
+CREATE VIEW ProductSupplierPrices AS
+SELECT 
+    p.ProductName,
+    p.CompatiblePrinterModels,
+    sp.SupplierID,
+    sp.SupplierProductPrice,
+    sp.SupplierMOQ,
+    sp.SupplierBulkDiscount
+FROM Product AS p
+LEFT JOIN SupplierPrice AS sp
+    ON p.ProductID = sp.ProductID
+   AND p.SupplierID = sp.SupplierID;
+
+   SELECT * FROM ProductSupplierPrices;
